e919752d3412347fe9c930e82cf8bd427832f3d9
claymccoy <git@claymccoy.com>
2023-02-06 10:59:55 -0600

    initial formula

---
+++ Formula/v1.0.8.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class V108 < Formula
+  desc "The CLI for Armory CD-as-a-Service."
+  homepage "https://github.com/armory-io/armory-cli"
+  url "https://github.com/armory-io/armory-cli/archive/refs/tags/v1.0.8.tar.gz"
+  sha256 "0938178b189438084b7c214ec07dd15f2a01adcfa6215ee518ffa49143f1137f"
+  license "Apache 2.0"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test v1.0.8`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
