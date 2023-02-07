890052ec7e28d7d1f464dce6bba0c893e1b3c35f
claymccoy <git@claymccoy.com>
2023-02-07 10:32:07 -0600

    rename armory execs

--- Formula/armory-cli.rb
+++ Formula/armory-cli.rb
@@ -10,7 +10,7 @@ class ArmoryCli < Formula
       sha256 "b29708b10d5456668f17b62179e9e6985e69943ca05999231af6d6617d5ca244"
 
       def install
-        bin.install "armory"
+        bin.install "armory-darwin-amd64" => "armory"
       end
     end
     if Hardware::CPU.arm?
@@ -18,7 +18,7 @@ class ArmoryCli < Formula
       sha256 "65be3ff2ec2b52cfb4b3f9a47adca16130b6e36efc025f47b8f6c1595a407539"
 
       def install
-        bin.install "armory"
+        bin.install "armory-darwin-arm64" => "armory"
       end
     end
   end
@@ -29,7 +29,7 @@ class ArmoryCli < Formula
       sha256 "5bc24022b033312e830bd60019223eab871ffcf48d15911d6d38ec10cc85a838"
 
       def install
-        bin.install "armory"
+        bin.install "armory-linux-amd64" => "armory"
       end
     end
     if Hardware::CPU.intel?
@@ -37,7 +37,7 @@ class ArmoryCli < Formula
       sha256 "7bb97841defb4bd6c828f512e94967c3915fc7a7783dd3819ee5228c64b480d5"
 
       def install
-        bin.install "armory"
+        bin.install "armory-linux-arm64" => "armory"
       end
     end
   end
