fa122735fc09eac42107dd5ca7736ca36ddd965c
th3morg <th3morg@users.noreply.github.com>
2023-03-09 18:39:30 +0000

    Update CLI Formula to 1.13.2

--- Formula/armory-cli.rb
+++ Formula/armory-cli.rb
@@ -1,21 +1,21 @@
 class ArmoryCli < Formula
   desc "CLI for Armory CD-as-a-Service"
   homepage "https://docs.armory.io/cd-as-a-service/setup/cli/"
-  version "1.13.1"
+  version "1.13.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.1/armory-darwin-amd64"
-      sha256 "62a2eac9a00ffb3bdb6f6dadff790bd0421c20de09c140ba6901c6916c0d07b8"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.2/armory-darwin-amd64"
+      sha256 "fac7196f467756d4c7430514953b1be19233a246fe126c291026ff2e66dda3f3"
 
       def install
         bin.install "armory-darwin-amd64" => "armory"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.1/armory-darwin-arm64"
-      sha256 "376cf098c9aab50e85367ff2de963f36bb15b6622651dff131fcf8c761638236"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.2/armory-darwin-arm64"
+      sha256 "18e1dfde6786cca9611eec315f2e8afd51c4be520385020e2262db2ab91b8ada"
 
       def install
         bin.install "armory-darwin-arm64" => "armory"
@@ -25,16 +25,16 @@ class ArmoryCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.1/armory-linux-arm64"
-      sha256 "753a8cc18f5ef60ac3e926fff0bd29317c32013c85868285203f5a41c88ed269"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.2/armory-linux-arm64"
+      sha256 "b8b0d27e2c7ba5f3b060582d922c72a04e19baf61afe0398b3dd15322f093cab"
 
       def install
         bin.install "armory-linux-arm64" => "armory"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.1/armory-linux-amd64"
-      sha256 "f94f494f1576df998f0d9087be02a4468b12ea31478c3e2c4b901a3c4f0c4fd6"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.13.2/armory-linux-amd64"
+      sha256 "264903777ca5c8dc25257145f206cffe7185a5f1bb53272a95c22a2a8d4d7b54"
 
       def install
         bin.install "armory-linux-amd64" => "armory"
