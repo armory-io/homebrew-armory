f5cbd6670987294751f99177e70c72da7e00ec0c
armory-platform <armory-platform@users.noreply.github.com>
2023-06-28 23:39:27 +0000

    Update CLI Formula to 1.15.1

--- Formula/armory-cli.rb
+++ Formula/armory-cli.rb
@@ -1,21 +1,21 @@
 class ArmoryCli < Formula
   desc "CLI for Armory CD-as-a-Service"
   homepage "https://docs.armory.io/cd-as-a-service/setup/cli/"
-  version "1.15.0"
+  version "1.15.1"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.0/armory-darwin-amd64"
-      sha256 "f2c70d2b0550630536478c5570445ad885f3cace3d9b800e68d493735fef1706"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.1/armory-darwin-amd64"
+      sha256 "0eff75edf65c717843a4cf144953739cb99772564f98540ca8251a1b85ef9847"
 
       def install
         bin.install "armory-darwin-amd64" => "armory"
       end
     end
     if Hardware::CPU.arm?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.0/armory-darwin-arm64"
-      sha256 "852e2a5bf3f32cdd4380b64c0a1fd09458ad317db2f4d34670fb30677a49545f"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.1/armory-darwin-arm64"
+      sha256 "3ff0d557d401876d876e964e108e234c99c957160ecee673db7583b909ad9cda"
 
       def install
         bin.install "armory-darwin-arm64" => "armory"
@@ -25,16 +25,16 @@ class ArmoryCli < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.0/armory-linux-arm64"
-      sha256 "85bbcf4920a8046f8b6d3218e66f29a9e0990a4e6ca6876fefe28eab28681312"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.1/armory-linux-arm64"
+      sha256 "cf638849458659948a54088a77936002e6e2aaf8dc54fd9ffbdc5784f39eb7ca"
 
       def install
         bin.install "armory-linux-arm64" => "armory"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.0/armory-linux-amd64"
-      sha256 "07ca86bf52cf02e9e600a5e88cddea807da5e0852b765ce98dd1a4a535049634"
+      url "https://github.com/armory-io/armory-cli/releases/download/v1.15.1/armory-linux-amd64"
+      sha256 "a2b589e659d4727e19cc67e8766930951b960f61538d80ae01f8da716133ef6d"
 
       def install
         bin.install "armory-linux-amd64" => "armory"
